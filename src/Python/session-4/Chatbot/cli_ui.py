fd937bba3f488f89ba8a5a6473524e50fd4a656f
Mohamed Hussein <mohamedoail1113@gmail.com>
2026-08-14 16:54:22 +0300

    chatbot

---
+++ src/Python/session-4/Chatbot/cli_ui.py
@@ -0,0 +1,12 @@
+from gen_fun import get_response
+
+def main_bot():
+    print("chatbot : Hi How can i assist you ?")
+    
+    while True:
+        user_input = input("User:   ").lower()
+        response = get_response(user_input)
+        print("chatbot:", response)
+        
+        if user_input == "goodbye":
+            break
